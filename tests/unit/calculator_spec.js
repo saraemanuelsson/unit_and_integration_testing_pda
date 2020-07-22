7ff80bfac0c33d9282a3c6691e51d0444c345047
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-22 15:20:22 +0100

    Add unit tests for click functions

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -14,40 +14,51 @@ describe('calculator', function () {
   it("should add numbers", function(){
     calculator.previousTotal = 1;
     calculator.add(4);
-    assert.strictEqual(5, calculator.runningTotal);
+    assert.strictEqual(calculator.runningTotal, 5);
   });
 
   it("should subtract numbers", function(){
     calculator.previousTotal = 7;
     calculator.subtract(4);
-    assert.strictEqual(3, calculator.runningTotal);
+    assert.strictEqual(calculator.runningTotal, 3);
   });
 
   it("should multiply numbers", function(){
     calculator.previousTotal = 3;
     calculator.multiply(5);
-    assert.strictEqual(15, calculator.runningTotal);
+    assert.strictEqual(calculator.runningTotal, 15);
   });
 
   it("should divide numbers", function(){
     calculator.previousTotal = 21;
     calculator.divide(3);
-    assert.strictEqual(7, calculator.runningTotal);
+    assert.strictEqual(calculator.runningTotal, 7);
   });
 
   it("should concatenate numbers on multiple number button clicks", function(){
     calculator.numberClick(1);
     calculator.numberClick(3);
     calculator.numberClick(5);
-    assert.strictEqual(135, calculator.runningTotal);
+    assert.strictEqual(calculator.runningTotal, 135);
   });
 
-  // it("should chain multiple operations together", function(){
+  it("should chain multiple operations together", function(){
+    calculator.numberClick(5);
+    calculator.operatorClick("+");
+    calculator.numberClick(2);
+    calculator.operatorClick('*');
+    calculator.numberClick(3);
+    calculator.operatorClick('=');
+    assert.strictEqual(calculator.runningTotal, 21);
+  });
 
-  // });
-
-  // it("should clear running total without affecting calculation", function(){
-
-  // });
+  it("should clear running total without affecting calculation", function(){
+    calculator.numberClick(5);
+    calculator.operatorClick("+");
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    calculator.clearClick();
+    assert.strictEqual(0, calculator.runningTotal)
+  });
 
 });
